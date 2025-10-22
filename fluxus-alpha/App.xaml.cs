b93a7f5f0ef085afb06e3a5e13608468bd068abb
Vitor Wille <vitorviciado296@gmail.com>
2025-10-22 14:19:46 -0300

    Adicionado arquivos inicias de desenvolvimento.

---
+++ fluxus-alpha/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace fluxus_alpha
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
