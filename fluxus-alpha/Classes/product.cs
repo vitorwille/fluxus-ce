a7cb71899855f4279ae682dd1771ecdb43aede34
Vitor Wille <vitorviciado296@gmail.com>
2025-10-23 15:20:36 -0300

    Adicionada classe de produtos e linkada ao datagrid
    Adicionado produtos para teste de exibição; modificada aparência do datagrid, contando com linhas zebradas e headers em negrito para melhor visibilidade.
    
    Adicionado emojis coloridos nos botões por meio de imagens para facilitar visualização do usuário.

---
+++ fluxus-alpha/Classes/product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fluxus_alpha.Classes
+{
+    public class Product
+    {
+        public int idProduto { get; set; }
+        public string produto { get; set; }
+        public string obsProduto { get; set; }
+        public int qtdProduto { get; set; }
+        public float valorProduto { get; set; }
+
+        public float valorTotalProduto => qtdProduto * valorProduto;
+    }
+}
--- fluxus-alpha/MainWindow.xaml.cs
+++ fluxus-alpha/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using fluxus_alpha.Classes;
+using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,41 @@ namespace fluxus_alpha
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Product> Produtos { get; set; } // importa dados da classe
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Produtos = new ObservableCollection<Product>
+            {
+                new Product
+                {
+                    idProduto = 1,
+                    produto = "Tecido Azul",
+                    obsProduto = "Cor azul royal",
+                    qtdProduto = 10,
+                    valorProduto = 15.5f
+                },
+                new Product
+                {
+                    idProduto = 2,
+                    produto = "Linha Preta",
+                    obsProduto = "100% algodão",
+                    qtdProduto = 25,
+                    valorProduto = 4.2f
+                },
+                new Product
+                {
+                    idProduto = 3,
+                    produto = "Teste foda",
+                    obsProduto = "te amo Sarah <3",
+                    qtdProduto = 999,
+                    valorProduto = 178.99f
+                }
+            };
+
+            dgProdutos.ItemsSource = Produtos;
         }
     }
 }
